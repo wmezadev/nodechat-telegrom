a83c72f6592b28e45d55511c30e25d739d1f41cf
William Eduardo Meza <williameduardomeza@gmail.com>
2020-01-04 15:00:14 -0500

    Se agrega metodo PATCH para actualizar un mensaje por su ID

--- components/message/controller.js
+++ components/message/controller.js
@@ -20,10 +20,22 @@ function addMessage(user, message) {
 function getMessages () {
     return new Promise ((resolve, reject) => {
         resolve(store.list());
-    })
+    });
+}
+
+function updateMessage (id, message) {
+    return new Promise (async (resolve, reject) => {
+        if(!id || !message){
+            reject('Invalid data');
+            return false;
+        }
+        const result = await store.updateMessage(id, message);
+        resolve(result);
+    });
 }
 
 module.exports = {
     addMessage,
-    getMessages
+    getMessages,
+    updateMessage
 };
--- components/message/network.js
+++ components/message/network.js
@@ -23,4 +23,15 @@ router.post('/', function(req, res) {
     });
 });
 
+router.patch('/:id', function(req, res) {
+    console.log(req.params.id);
+    controller.updateMessage(req.params.id, req.body.message)
+        .then((data) => {
+            response.success(req, res, data, 200);
+        })
+        .catch((e) => {
+            response.error(req, res, 'Error interno', 500, e);
+        });
+});
+
 module.exports = router;
