62ef1b6abd357c2df3106136b7e5fcec8d5854b2
William Eduardo Meza <williameduardomeza@gmail.com>
2020-01-04 11:40:31 -0500

    Se inicializa proyecto de curso de node

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = require('./network/routes');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+router(app);
+
+app.use('/app', express.static('public'));
+app.listen(3000);
+console.log('La aplicación está escuchando en http://localhost:3000');
