b9fb7ece05238b70569254321e8e8d75228dc712
William Eduardo Meza <williameduardomeza@gmail.com>
2020-01-05 21:23:39 -0500

    Se agrega cors para conectar con frontend endpoint

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const socket = require('./socket');
 const db = require('./db');
 const router = require('./network/routes');
 
 db.connect(process.env.DB_URI);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
